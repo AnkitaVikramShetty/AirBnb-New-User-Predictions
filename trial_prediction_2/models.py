d049466488a42fbf21659defd9d501db767bec3a
maazsyed <smaaz015@gmail.com>
2017-04-12 19:12:47 -0400

    Created countries model for the trial_training_2 app.

--- trial_prediction_2/models.py
+++ trial_prediction_2/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class countries(models.Model):
+    country_destination = models.CharField(max_length=2)
+    lat_destination = models.DecimalField()
+    lng_destination = models.DecimalField()
+    distance_km = models.DecimalField()
+    destination_km2 = models.IntegerField()
+    destination_language = models.CharField(max_length=3)
+    language_levenshtein_distance = models.FloatField()
+
+# album = models.Foreignkey(Album, on_delete = models.CASCADE)
