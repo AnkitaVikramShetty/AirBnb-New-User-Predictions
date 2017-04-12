3a77c66de600f1e70c4d9bb5725f3728cc1c1814
Ankita Shetty <shettya@iu.edu>
2017-04-12 19:14:00 -0400

    Creating a model 'countries' for trial_predictions.

--- trial_predictions/models.py
+++ trial_predictions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class countries(models.Model):
+    country_destination = models.CharField(max_length=2)
+    lat_destination = models.FloatField()
+    lng_destination = models.FloatField()
+    distance_km = models.FloatField()
+    destination_km2 = models.IntegerField
+    destination_language = models.CharField(max_length=3)
+    language_levenshtein_distance = models.FloatField()
