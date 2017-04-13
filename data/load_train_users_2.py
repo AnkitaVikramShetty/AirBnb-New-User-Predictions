fc4c4c900214846e6956f768d6a0683400b4b107
Ankita Shetty <shettya@iu.edu>
2017-04-13 15:30:06 -0400

    1. Creating python script for loading 'train_users_2' data to the database.

---
+++ data/load_train_users_2.py
@@ -0,0 +1,42 @@
+import sys, os
+import csv
+
+#csv.field_size_limit(sys.maxsize)
+csv.field_size_limit(2147483647)
+
+# Full path and name to your csv file
+csv_filepathname="train_users_2.csv"
+# Full path to your django project directory
+your_djangoproject_home="/Users/AnkitaShetty/PycharmProjects/airbnbNewUserPredictions/"
+
+
+sys.path.append(your_djangoproject_home)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'airbnbNewUserPredictions.settings'
+
+from trial_prediction_2.models import train_users_2, countries
+
+
+dataReader = csv.reader(open(csv_filepathname), delimiter=',', quotechar='"')
+
+count = 0
+for row in dataReader:
+    count += 1
+    if row[0] != 'id' and row[15] != 'NDF' and row[15] != 'other': # Ignore the header row, import everything else
+        print(count)
+        train_user_2 = train_users_2()
+        train_user_2.id = row[0]
+        train_user_2.date_account_created = row[1]
+        train_user_2.timestamp_first_active = row[2]
+        train_user_2.date_first_booking = row[3]
+        train_user_2.gender = row[4]
+        train_user_2.age = row[5]
+        train_user_2.signup_method = row[6]
+        train_user_2.signup_flow = row[7]
+        train_user_2.language = row[8]
+        train_user_2.affiliate_channel = row[9]
+        train_user_2.affiliate_provider = row[10]
+        train_user_2.first_affiliate_tracked = row[11]
+        train_user_2.signup_app = row[12]
+        train_user_2.first_device_type = row[13]
+        train_user_2.first_browser = row[14]
+        train_user_2.country_destination = countries.objects.get(country_destination = row[15])
